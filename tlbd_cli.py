c089b26743eab8f8ffe0c5cd579595a2e5ee42d4
Anusha Datar <anushadatar@gmail.com>
2020-03-24 13:51:01 -0400

    Separating utility files.

---
+++ tlbd_cli.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+CLI interface for utility functions.
+"""
+# TODO Add Functionality to clear dataset from local files.
+
+import argparse
+import getpass
+import os
+import pandas as pd
+import tinyapi
+
+from cli_utils import *
+
+def main():
+    """
+    Offers command-line options for pulling and working with that 
+    sweet treasure trove of tinyletter data.
+    """
+    commands = argparse.ArgumentParser()
+    commands.add_argument("-u", "--update", help="Pulls data down \
+                          from the tinyletter servers, processes it, and \
+                          stores it in CSV files.", 
+                          action="store_true");
+    commands.add_argument("-a", "--all", help="Gives all time stats \
+                          from every issue.", action="store_true") 
+    commands.add_argument("-i", "--issue", help="Gives stats for \
+                          some issue i of the newsletter (0-indexed.", \
+                          action="store_true")
+    arguments = commands.parse_args()
+    if arguments.update:
+        pull_data()
+    if arguments.all:
+        all_time_stats()
+    if arguments.issue:
+        itemized_stats()
+
+if __name__ == '__main__':
+    main()
